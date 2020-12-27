c622aa9efc1364d66507a606ac5a7c3cd83c5b5d
Tom Bignall <bignallt@gmail.com>
2020-12-26 17:28:02 -0800

    Prefinal Update

--- js/script.js
+++ js/script.js
@@ -11,20 +11,171 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+/**
+ List of selected elements and variable delcarations
+ */
+const header = document.querySelector(".header");
+let searchDiv = document.createElement('div');
+searchDiv.id = "searchBarDiv";
+searchDiv.innerHTML = `
+   <label for="search" class="student-search">
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>
+`;
+header.appendChild(searchDiv);
+const button = header.querySelector("#searchBarDiv button");
+const input = header.querySelector("#search");
+
+let ulOfStudents = document.querySelector('.student-list');
+
+const itemsPerPage = 9;
 
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+/**
+ * `showPage` function
+ * 
+ * Description: This function will create and insert/append the elements needed to display a "page" of nine students.
+ * 
+ * @param { Array } list - Of data to display
+ * @param { Number } page - A provided page number to perform Math and provide correct amount of cards
+ * 
+ * @returns { output } - Performs task of displaying elements to the page. 
 */
 
+function showPage(list, page) {
+   const startIndex = (page * itemsPerPage ) - itemsPerPage; 
+   const endIndex = page * itemsPerPage;
+   ulOfStudents.innerHTML = "";
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         let li = `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                  <span class="email">${list[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">${list[i].registered.date}</span>
+               </div>
+            </li>
+         `;
+         ulOfStudents.insertAdjacentHTML("beforeend", li);
+      }    
+   }
+}
 
+   
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+/** 
+ * `addPagination` function
+ * 
+ * Description: This function will create and insert/append the elements needed for the pagination buttons.
+ *              Displays all buttons to the page, and then adds and removes an active class
+ *              Note: Event Listner used on the created UL to update the buttons
+ * 
+ * @param { array } list - Data, specifically length of data to create required buttons
+ * 
+ * @returns { output } - Reloads the showPage function update display. IMPORTANT sends an updated second param
+ * 
 */
 
+function addPagination(list) {
+   // Math.ciel because we will need extras on final page
+   const numOfButtons = Math.ceil( list.length / itemsPerPage );           
+   let ulPagination = document.querySelector('.link-list');
+   ulPagination.innerHTML = "";
+
+   // Loop through the required number of buttons, to create buttons
+   for (let i = 0; i < numOfButtons; i++) {
+      let li = `
+         <li>
+            <button type="button">${i + 1}</button>
+         </li>
+      `;
+      ulPagination.insertAdjacentHTML("beforeend", li);  
+   }
+   
+   // Traverse to the UL to get to the first button and add an active class
+   if (list.length > 0) {
+      let liList = ulPagination.firstElementChild;
+      let firstButton = liList.firstElementChild;
+      firstButton.className = "active";
+   } else {
+      ulOfStudents.innerHTML = "<h1 style='text-align: center; font-size: 50px;'>No Items to Display</h1>";
+   }
+   
+  
+   // Function Event Listener: - Listening for button clicks, remove active class and reassign to clicked button
+   ulPagination.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") {
+         let clickedButton = e.target;
+         for (let i = 0; i < numOfButtons; i++) {
+            let button = ulPagination.children[i].firstElementChild;
+            button.classList.remove('active')
+         }
+         clickedButton.className = "active";
+
+         // Reload the 'showPage' function to update page display
+         showPage(list, clickedButton.textContent);
+         }
+   });
+}
+
+/**
+ * `performSearch` function
+ * 
+ * Description: 
+ * 
+ * @param {*} inputValue 
+ * @param {*} names 
+ */
 
 
+function performSearch(inputValue, names) {
+   let arrayAccept = [];
+   for (let i = 0; i < names.length; i++) {
+      let fullName = `${names[i].name.first.toLowerCase()} ${names[i].name.last.toLowerCase()}`;
+      if (fullName.includes(inputValue) ) {
+         arrayAccept.push(names[i]);
+      }
+   }
+   showPage(arrayAccept, 1);
+   addPagination(arrayAccept);
+}
+
+
+
+input.addEventListener('keyup', () => {
+   performSearch(input.value.toLowerCase(), data);
+});
+
+button.addEventListener('submit', () => {
+   performSearch(input.value.toLowerCase(), data);
+});
+
+
+
+   
 // Call functions
+showPage(data, 1);
+addPagination(data);
+
+// Notes and practice
+
+// COULD ALSO USE:
+// let li =  `
+//             <li class="student-list cf">
+//             <div class="student-details">
+//                <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+//                <h3>${list[i].name.first} ${list[i].name.last}</h3>
+//                <span class="email">${list[i].email}</span>
+//             </div>
+//             <div class="joined-details">
+//                <span class="date">${list[i].registered.date}</span>
+//             </div>
+//             </li>
+//          `;
+
+//          ul.insertAdjecentHTML('beforeend', li);
