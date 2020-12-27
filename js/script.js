086d81b2a0ca4088676dd2d00ecf9c6c2c98abc7
Tom Bignall <bignallt@gmail.com>
2020-12-26 21:18:18 -0800

    Commit before review

--- js/script.js
+++ js/script.js
@@ -3,19 +3,17 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-/**
- List of selected elements and variable delcarations
- */
+/*************
+ * List of selected elements and variable delcarations
+ ************/
 const header = document.querySelector(".header");
-let searchDiv = document.createElement('div');
+let searchDiv = document.createElement("div");
 searchDiv.id = "searchBarDiv";
 searchDiv.innerHTML = `
    <label for="search" class="student-search">
@@ -27,10 +25,14 @@ header.appendChild(searchDiv);
 const button = header.querySelector("#searchBarDiv button");
 const input = header.querySelector("#search");
 
-let ulOfStudents = document.querySelector('.student-list');
+// Created this UL because "student-list" used in more than one function
+let ulOfStudents = document.querySelector(".student-list");
 
 const itemsPerPage = 9;
 
+/*************
+ * Functions
+ ************/
 
 /**
  * `showPage` function
@@ -57,7 +59,7 @@ function showPage(list, page) {
                   <span class="email">${list[i].email}</span>
                </div>
                <div class="joined-details">
-                  <span class="date">${list[i].registered.date}</span>
+                  <span class="date">Joined ${list[i].registered.date}</span>
                </div>
             </li>
          `;
@@ -65,8 +67,6 @@ function showPage(list, page) {
       }    
    }
 }
-
-   
 
 /** 
  * `addPagination` function
@@ -82,9 +82,9 @@ function showPage(list, page) {
 */
 
 function addPagination(list) {
-   // Math.ciel because we will need extras on final page
+   // Math.ciel because we will need a page for remainder (spillover) data
    const numOfButtons = Math.ceil( list.length / itemsPerPage );           
-   let ulPagination = document.querySelector('.link-list');
+   let ulPagination = document.querySelector(".link-list");
    ulPagination.innerHTML = "";
 
    // Loop through the required number of buttons, to create buttons
@@ -97,23 +97,25 @@ function addPagination(list) {
       ulPagination.insertAdjacentHTML("beforeend", li);  
    }
    
-   // Traverse to the UL to get to the first button and add an active class
+   /*
+   Traverse to the UL to get to the first button and add an active
+   ONLY IF there is more than one character entered
+   */
    if (list.length > 0) {
       let liList = ulPagination.firstElementChild;
       let firstButton = liList.firstElementChild;
       firstButton.className = "active";
    } else {
-      ulOfStudents.innerHTML = "<h1 style='text-align: center; font-size: 50px;'>No Items to Display</h1>";
+      ulOfStudents.innerHTML = "<h1 class='no-results'>No Results to Display</h1>";
    }
-   
-  
+    
    // Function Event Listener: - Listening for button clicks, remove active class and reassign to clicked button
    ulPagination.addEventListener("click", (e) => {
       if (e.target.tagName === "BUTTON") {
          let clickedButton = e.target;
          for (let i = 0; i < numOfButtons; i++) {
             let button = ulPagination.children[i].firstElementChild;
-            button.classList.remove('active')
+            button.classList.remove("active")
          }
          clickedButton.className = "active";
 
@@ -124,14 +126,15 @@ function addPagination(list) {
 }
 
 /**
+ *  EXTRA CREDIT
  * `performSearch` function
  * 
- * Description: 
+ * Description: Accept input from the user to match it to data.js
+ *             Everytime function is executed, it creates a local array that stores names that match the input
  * 
- * @param {*} inputValue 
- * @param {*} names 
+ * @param { string } inputValue - Letter(s) entered by the user in the searchbox 
+ * @param {*} names - List of data from data.js
  */
-
 
 function performSearch(inputValue, names) {
    let arrayAccept = [];
@@ -141,41 +144,28 @@ function performSearch(inputValue, names) {
          arrayAccept.push(names[i]);
       }
    }
+
+   // Run both showPage and addPagination functions with the new, local array
    showPage(arrayAccept, 1);
    addPagination(arrayAccept);
 }
 
+/*************
+ * Event Listeners 
+ * 1 x every key press
+ * 1 x when the button in pressed (or enter clicked)
+*************/
 
-
-input.addEventListener('keyup', () => {
+input.addEventListener("keyup", () => {
    performSearch(input.value.toLowerCase(), data);
 });
 
-button.addEventListener('submit', () => {
+button.addEventListener("submit", () => {
    performSearch(input.value.toLowerCase(), data);
 });
 
-
-
-   
-// Call functions
+/*************
+ * Function calls to set up the page
+ ************/
 showPage(data, 1);
 addPagination(data);
-
-// Notes and practice
-
-// COULD ALSO USE:
-// let li =  `
-//             <li class="student-list cf">
-//             <div class="student-details">
-//                <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
-//                <h3>${list[i].name.first} ${list[i].name.last}</h3>
-//                <span class="email">${list[i].email}</span>
-//             </div>
-//             <div class="joined-details">
-//                <span class="date">${list[i].registered.date}</span>
-//             </div>
-//             </li>
-//          `;
-
-//          ul.insertAdjecentHTML('beforeend', li);
